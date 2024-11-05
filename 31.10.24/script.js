32b988d3e721780ece16fe7230b47610de2acb0e
SyrymSS <malalibek940@gmail.com>
2024-11-05 12:52:48 +0500

    homework

---
+++ 31.10.24/script.js
@@ -0,0 +1,13 @@
+let slicesPerPizza = 8;
+let numberOfPizzas = 5;
+let totalSlices = slicesPerPizza * numberOfPizzas;
+console.log("Барлық тілімдер саны:", totalSlices);
+
+let totalChocolates = 24;
+let numberOfFriends = 6;
+let chocolatesPerFriend = totalChocolates / numberOfFriends;
+console.log("Әрбір досқа шоколад саны:", chocolatesPerFriend);
+
+let plantHeight = 10;
+plantHeight += 2;
+console.log("Бір аптадан кейінгі өсімдік биіктігі:", plantHeight);
